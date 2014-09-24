8c286da5c598bcf3034544b59de2abaf66f6ca22
Sam Halperin <sam@samhalperin.com>
2014-09-24 16:34:49 -0400

    working on extruding shapes.

---
+++ extrusion/Assets/Extrusion.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace Geom
+
+{
+	public class Extrusion {
+		private Vector3[] vertecies;
+		private int[] triangles;
+		
+		public Extrusion(Vector3[] vertecies, int[] triangles) {
+			this.vertecies = vertecies;
+			this.triangles = triangles;
+		}
+		
+		public static Extrusion ExtrudeY(Vector3[] shape, float height) {
+			// First double the vertex array with extruded y values
+			Vector3[] V = new Vector3[shape.Length * 2];
+			int s = shape.Length;
+			for (int i = 0; i<s; i++) {
+				V[i] = shape[i];
+				V[i+s] = shape[i];
+				V[i+s].y = V[i].y + height;
+			}
+			
+
+			// Next create the sides, an array of triangle vertecies.  Every 3 indecies in the array
+			// is a triangle.
+			List<int> _T = new List<int>();
+			
+			
+			for (int i = 0; i < s; i++) {
+				int v0 = i;
+				int v1;
+				int v2;
+				int v3;
+				
+				if ((i + 1) < s) 
+				{
+					v1 = i+1;
+				} else {
+					v1 = 0;
+				}
+				v2 = v1 + s;
+				v3 = v0 + s;
+				
+
+				_T.Add(v0);
+				_T.Add(v1);
+				_T.Add(v2);
+				
+				_T.Add(v0);
+				_T.Add(v2);
+				_T.Add(v3);
+			}
+			
+			// next triangulate the orignal poly to get end cap indecies
+			Vector2[] _base = new Vector2[s];
+			for (var i = 0; i<s; i++) 
+				_base[i] = new Vector2(shape[i].x, shape[i].z);
+			Triangulator tr = new Triangulator(_base);
+			int[] _baseIndecies = tr.Triangulate();
+
+
+
+			foreach (int i in _baseIndecies)   //top cap
+				_T.Add (s + i);
+
+			Array.Reverse (_baseIndecies);
+
+			foreach (int i in _baseIndecies)// bottom cap
+			{
+				_T.Add(i);
+			}
+
+
+
+			int[] T = _T.ToArray ();
+			return new Extrusion(V, T);
+			
+		}
+		
+		public void LoadMesh(Mesh mesh) {
+			mesh.Clear();
+			mesh.vertices = this.vertecies;
+			mesh.triangles = this.triangles;
+			mesh.RecalculateNormals();
+		}
+		
+	}
+}
---
+++ extrusion/Assets/ExtrusionTest.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Geom;
+
+public class ExtrusionTest : MonoBehaviour {
+	private static Vector3[] TEST_POLY1 = {
+		new Vector3(4,0,4),
+		new Vector3(4,0,5),
+		new Vector3(5,0,5),
+		new Vector3(5,0,4)
+	};
+	private static Vector3[] TEST_POLY2 = {
+		new Vector3(0,0,0),
+		new Vector3(1,0,1),
+		new Vector3(2,0,0)
+	};
+	private static Vector3[] TEST_POLY3 = {
+		new Vector3(0,0,0),
+		new Vector3(0,0,2),
+		new Vector3(2,0,2),
+		new Vector3(2,0,1),
+		new Vector3(1,0,1),
+		new Vector3(1,0,0),
+	};
+
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3[][] shapes = {TEST_POLY1, TEST_POLY3};
+
+		foreach (Vector3[] shape in shapes) 
+		{
+			GameObject o = new GameObject();
+			o.AddComponent("MeshFilter");
+			o.AddComponent("MeshRenderer");
+			Extrusion e = Extrusion.ExtrudeY(shape, 1.0f);
+			e.LoadMesh(o.GetComponent<MeshFilter>().mesh);
+		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
